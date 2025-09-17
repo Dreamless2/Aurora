1380af9352a58f9560aed9bd7e247ee7c5677399
Tiago Pereira <tiagonet50@gmail.com>
2025-09-17 19:13:59 -0300

    Atualizado Index.cshtml

--- Controllers/EnderecoController.cs
+++ Controllers/EnderecoController.cs
@@ -8,5 +8,20 @@ namespace Aurora.Controllers
         {
             return View();
         }
+        
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult Excluir()
+        {
+            return View();
+        }
     }
 }
