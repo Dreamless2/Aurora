d1488fd312a7bb905d37a53366ebc05e81b3c039
Tiago Pereira <tiagonet50@gmail.com>
2025-09-18 19:45:29 -0300

    Atualizado Criar.cshtml

--- Models/EnderecoModel.cs
+++ Models/EnderecoModel.cs
@@ -3,6 +3,7 @@
     public class EnderecoModel
     {
         public int Id { get; set; }
+        public string? Nome { get; set; }
         public string? Endereco { get; set; }
         public string? Numero { get; set; }
         public string? Complemento { get; set; }
