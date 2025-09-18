911c7c4867d9de6d62af09f10c0179c730cbc413
Tiago Pereira <tiagonet50@gmail.com>
2025-09-18 19:36:50 -0300

    Create EnderecoRepositorio.cs

---
+++ Repositorio/EnderecoRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using Aurora.Data;
+using Aurora.Models;
+
+namespace Aurora.Repositorio
+{
+    public class EnderecoRepositorio : IEnderecoRepositorio
+    {
+        private readonly BancoContext _bancoContext;
+        public EnderecoRepositorio(BancoContext bancoContext)
+        {
+            _bancoContext = bancoContext;
+
+        }
+        public EnderecoModel Adicionar(EnderecoModel endereco)
+        {
+            _bancoContext.Enderecos.Add(endereco);
+            _bancoContext.SaveChanges();
+            return endereco;
+        }
+    }
+}
