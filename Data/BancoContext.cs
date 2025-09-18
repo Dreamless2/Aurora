83bfe104b86dc3eb2ee19bba441cb5cc6a71d17d
Tiago Pereira <tiagonet50@gmail.com>
2025-09-18 18:59:32 -0300

    Atualizado Aurora.csproj BancoContext.cs

--- Data/BancoContext.cs
+++ Data/BancoContext.cs
@@ -4,5 +4,9 @@ namespace Aurora.Data
 {
     public class BancoContext: DbContext
     {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options)
+        {
+
+        }
     }
 }
