b6e0ffad9dbb3f3c76f9244595f8e7fbbd98eb2f
Tiago Pereira <tiagonet50@gmail.com>
2025-09-18 18:20:09 -0300

    Atualizado EnderecoModel.cs

--- Models/EnderecoModel.cs
+++ Models/EnderecoModel.cs
@@ -2,5 +2,14 @@
 {
     public class EnderecoModel
     {
+        public int Id { get; set; }
+        public string? Endereco { get; set; }
+        public string? Numero { get; set; }
+        public string? Complemento { get; set; }
+        public string? Bairro { get; set; }
+        public string? Cidade { get; set; }
+        public string? Estado { get; set; }
+        public string? Cep { get; set; }
+        public string? Telefone { get; set; }
     }
 }
