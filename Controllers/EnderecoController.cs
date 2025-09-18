c7c61af7991bb915c7a09e2bac6d10ae3d19aee2
Tiago Pereira <tiagonet50@gmail.com>
2025-09-18 19:40:30 -0300

    Atualizado EnderecoRepositorio.cs

--- Controllers/EnderecoController.cs
+++ Controllers/EnderecoController.cs
@@ -1,14 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Aurora.Repositorio;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Aurora.Controllers
 {
     public class EnderecoController : Controller
     {
+        private readonly IEnderecoRepositorio _enderecoRepositorio;
+        public EnderecoController(IEnderecoRepositorio enderecoRepositorio)
+        {
+            _enderecoRepositorio = enderecoRepositorio;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
-        
+
         public IActionResult Criar()
         {
             return View();
@@ -23,5 +30,13 @@ namespace Aurora.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Criar(Models.EnderecoModel enderecoModel)
+        {
+            _enderecoRepositorio.Adicionar(enderecoModel);
+            return RedirectToAction("Index");
+        }
+
     }
 }
