54b3742161f3608d08845856e86b2091fec0e912
Tiago Pereira <tiagonet50@gmail.com>
2025-09-18 21:57:29 -0300

    Atualizado BancoContext.cs

--- Data/BancoContext.cs
+++ Data/BancoContext.cs
@@ -10,22 +10,4 @@ namespace Aurora.Data
 
     }
 
-        public DbSet<Models.EnderecoModel> Enderecos { get; set; }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            // Configurar a tabela "enderecos"
-            modelBuilder.Entity<EnderecoModel>(entity =>
-            {
-                entity.ToTable("enderecos", b => b.UseSqlOutputClause(false)); // 🔹 Desabilita OUTPUT para evitar erro com trigger
-
-                // Configura o campo updated_at como gerado pelo banco
-                entity.Property(e => e.UpdatedAt)
-                      .HasColumnName("updated_at")
-                      .ValueGeneratedOnAddOrUpdate()
-                      .Metadata.SetAfterSaveBehavior(PropertySaveBehavior.Ignore);
-            });
-        }
-    }
 }
