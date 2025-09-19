f89345df661aaa9d3e52aff458935ddab70be493
Tiago Pereira <tiagonet50@gmail.com>
2025-09-18 22:24:41 -0300

    Atualizado EnderecoController.cs Criar.cshtml

--- Models/EnderecoModel.cs
+++ Models/EnderecoModel.cs
@@ -5,22 +5,22 @@ namespace Aurora.Models
     public class EnderecoModel
     {
         public int Id { get; set; }
-        [Required(ErrorMessage = "O campo Nome é obrigatório")]
+        [Required(ErrorMessage = "Informe o nome")]
         public string? Nome { get; set; }
-        [Required(ErrorMessage = "O campo Endereço é obrigatório")]
+        [Required(ErrorMessage = "Informe o endereço")]
         public string? Endereco { get; set; }
-        [Required(ErrorMessage = "O campo Número é obrigatório")]
+        [Required(ErrorMessage = "Informe o número")]
         public string? Numero { get; set; }
         public string? Complemento { get; set; }
-        [Required(ErrorMessage = "O campo Bairro é obrigatório")]
+        [Required(ErrorMessage = "Informe o bairro")]
         public string? Bairro { get; set; }
-        [Required(ErrorMessage = "O campo Cidade é obrigatório")]
+        [Required(ErrorMessage = "Informe a cidade")]
         public string? Cidade { get; set; }
-        [Required(ErrorMessage = "O campo Estado é obrigatório")]
+        [Required(ErrorMessage = "Informe o estado")]
         public string? Estado { get; set; }
-        [Required(ErrorMessage = "O campo CEP é obrigatório")]
+        [Required(ErrorMessage = "Informe o CEP")]
         public string? Cep { get; set; }
-        [Required(ErrorMessage = "O campo Telefone é obrigatório")]
+        [Required(ErrorMessage = "Informe o telefone")]
         public string? Telefone { get; set; }
         public DateTime? UpdatedAt { get; set; }
     }
